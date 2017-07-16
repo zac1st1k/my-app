e644cbf2b9d838e841135cd33fbb7586638e1aa1
Zac <zacfirst@gmail.com>
2017-07-17 00:05:31 +1000

    feat: add reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReactiveFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.css']
+})
+export class ReactiveFormComponent implements OnInit {
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      address: this.formBuilder.group({
+        street: [],
+        zip: [],
+        city: []
+      })
+    });
+
+    this.registerForm.valueChanges.subscribe(value => {
+      console.log('registerForm changed!', this.registerForm)
+    })
+
+    this.registerForm.controls.firstname.valueChanges.subscribe(value => {
+      console.log('firstname changed!', this.registerForm.controls.firstname)
+    })
+  }
+}
