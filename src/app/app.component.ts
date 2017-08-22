4d53661ecae95857a76f80b6d5b043b9a87ce772
Zac <zacfirst@gmail.com>
2017-08-23 00:22:10 +1000

    chore: test types

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + ' ' + middleInitial + ' ' + lastName;
+    }
+}
+
+const user = new Student('Jane', 'M.', 'User');
+
+function greeter(person: Person) {
+    return 'Hello, ' + person.firstName + ' ' + person.lastName;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -13,6 +31,25 @@ export class AppComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    // console.log('rootModel', this.rootModel);
+    console.log(greeter(user)); // interface compatible
+    let num: number;
+    num = undefined; // undefined
+    const fibonacci: [] = [1, '1', 2, 3, 5];
+    console.log('rootModel', this.rootModel);
+    console.logj('fibonacci', fibonacci.push(true));
+    function buildName(firstName?: string, lastName: string) {
+        return firstName + ' ' + lastName;
+    }
+    const xcat = buildName(23, 43);
+    console.log('xcat', xcat(true));
+  }
+
+  test () {
+    const bar: {
+        foo: number | null
+    } = {};
+
+    badsdfr.bar.foo.length.length = 3;
+    console.log(bar.bar.foo.length);
   }
 }
